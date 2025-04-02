b38fdeeb162a983fe3fe8aa238c527a462812d7a
rakellbandeira <per22016l@byui.edu>
2025-04-01 23:21:22 -0400

    endpoints corrected

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'API for managing cooking recipes and user accounts',
     version: '1.0.0'
   },
-  host: 'render.com/docs/web-services#port-binding',
+  host: 'recipemgmtapi.onrender.com',
   schemes: ['https'],
   tags: [
     {
@@ -18,6 +18,266 @@ const doc = {
       description: 'Endpoints for managing users'
     }
   ],
+  paths: {
+    '/recipes': {
+      get: {
+        tags: ['Recipes'],
+        description: 'Get all recipes',
+        responses: {
+          '200': {
+            description: 'Successfully retrieved recipes'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      },
+      post: {
+        tags: ['Recipes'],
+        description: 'Create a new recipe',
+        parameters: [
+          {
+            in: 'body',
+            name: 'recipe',
+            description: 'Recipe object to be added',
+            required: true,
+            schema: {
+              $ref: '#/definitions/Recipe'
+            }
+          }
+        ],
+        responses: {
+          '201': {
+            description: 'Recipe created successfully'
+          },
+          '400': {
+            description: 'Invalid input'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      }
+    },
+    '/recipes/{id}': {
+      get: {
+        tags: ['Recipes'],
+        description: 'Get a recipe by ID',
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            description: 'Recipe ID',
+            required: true,
+            type: 'string'
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Successfully retrieved recipe'
+          },
+          '404': {
+            description: 'Recipe not found'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      },
+      put: {
+        tags: ['Recipes'],
+        description: 'Update a recipe',
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            description: 'Recipe ID',
+            required: true,
+            type: 'string'
+          },
+          {
+            in: 'body',
+            name: 'recipe',
+            description: 'Updated recipe object',
+            required: true,
+            schema: {
+              $ref: '#/definitions/Recipe'
+            }
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Recipe updated successfully'
+          },
+          '400': {
+            description: 'Invalid input'
+          },
+          '404': {
+            description: 'Recipe not found'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      },
+      delete: {
+        tags: ['Recipes'],
+        description: 'Delete a recipe',
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            description: 'Recipe ID',
+            required: true,
+            type: 'string'
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Recipe deleted successfully'
+          },
+          '404': {
+            description: 'Recipe not found'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      }
+    },
+    '/users': {
+      get: {
+        tags: ['Users'],
+        description: 'Get all users',
+        responses: {
+          '200': {
+            description: 'Successfully retrieved users'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      },
+      post: {
+        tags: ['Users'],
+        description: 'Create a new user',
+        parameters: [
+          {
+            in: 'body',
+            name: 'user',
+            description: 'User object to be added',
+            required: true,
+            schema: {
+              $ref: '#/definitions/User'
+            }
+          }
+        ],
+        responses: {
+          '201': {
+            description: 'User created successfully'
+          },
+          '400': {
+            description: 'Invalid input'
+          },
+          '409': {
+            description: 'Email already in use'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      }
+    },
+    '/users/{id}': {
+      get: {
+        tags: ['Users'],
+        description: 'Get a user by ID',
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            description: 'User ID',
+            required: true,
+            type: 'string'
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'Successfully retrieved user'
+          },
+          '404': {
+            description: 'User not found'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      },
+      put: {
+        tags: ['Users'],
+        description: 'Update a user',
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            description: 'User ID',
+            required: true,
+            type: 'string'
+          },
+          {
+            in: 'body',
+            name: 'user',
+            description: 'Updated user object',
+            required: true,
+            schema: {
+              $ref: '#/definitions/User'
+            }
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'User updated successfully'
+          },
+          '400': {
+            description: 'Invalid input'
+          },
+          '404': {
+            description: 'User not found'
+          },
+          '409': {
+            description: 'Email already in use'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      },
+      delete: {
+        tags: ['Users'],
+        description: 'Delete a user',
+        parameters: [
+          {
+            in: 'path',
+            name: 'id',
+            description: 'User ID',
+            required: true,
+            type: 'string'
+          }
+        ],
+        responses: {
+          '200': {
+            description: 'User deleted successfully'
+          },
+          '404': {
+            description: 'User not found'
+          },
+          '500': {
+            description: 'Server error'
+          }
+        }
+      }
+    }
+  },
   definitions: {
     Recipe: {
       title: "Chocolate Chip Cookies",
@@ -52,7 +312,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./routes/index.js', './routes/recipes.js', './routes/users.js'];
+const routes = ['./routes/recipes.js', './routes/users.js'];
 
 
 swaggerAutogen(outputFile, routes, doc);
